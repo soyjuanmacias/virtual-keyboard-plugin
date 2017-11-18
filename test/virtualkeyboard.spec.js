e1736191b70fa8572e7fc9846fbe924551e482ae
stephen <stephen.baroan@gmail.com>
2017-11-18 20:48:48 +0100

    setting  up karma for tests

---
+++ test/virtualkeyboard.spec.js
@@ -0,0 +1,16 @@
+// const virtualkeyboard = require('../src/VirtualKeyboard.js');
+/* global describe beforeEach it */
+import VirtualKeyboard from '../src/VirtualKeyboard';
+
+describe('virtualkeyboard', () => {
+
+  let document = null;
+  // beforeEach(() => {
+  //   document.
+  // });
+
+  it('should init the constructor', () => {
+    const virtualkeyboardInstance = new VirtualKeyboard();
+    console.log(virtualkeyboardInstance);
+  });
+});
